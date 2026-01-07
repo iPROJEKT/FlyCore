4d791f78545aff919cc6cc8fac2dfb341010d2df
Your Name <you@example.com>
2026-01-07 13:42:35 +0300

    add new method

--- scene_utils/__init__.py
+++ scene_utils/__init__.py
@@ -1,6 +1,7 @@
-from .primitives import create_grid_floor, create_box
+from .primitives import create_grid_floor, create_box, create_rect_box
 
 __all__ = [
     "create_grid_floor",
     "create_box",
+    "create_rect_box",
 ]
